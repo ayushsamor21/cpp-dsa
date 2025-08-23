7bbabda57955c117f278864ca7b9fb62b5465243
ayush samor <ayushsamor21@gmail.com>
2025-08-23 22:48:09 +0530

    stack with vector

---
+++ stack/2-basic-stack-with-vector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int capacity, value;
+    cout << "Please enter the size of stack: ";
+    cin >> capacity;
+
+    vector<int> intStack;
+
+    for (int i = 0; i < capacity; i++){
+        cout << "Please enter the element: ";
+        cin >> value;
+        intStack.push_back(value);
+    }
+
+    cout << intStack.back() << " popped from stack\n"; // access the top element
+    intStack.pop_back(); // remove the top element
+
+    cout << "Top element is: " << intStack.back() << endl;
+
+    cout << "Elements present in stack: ";
+    while (!intStack.empty()) {
+        cout << intStack.back() << " ";
+        intStack.pop_back();
+    }
+
+    return 0;
+}
